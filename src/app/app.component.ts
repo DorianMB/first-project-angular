f7bd629da9021e7a726e0923a2b40c824885c0b6
Dorian MARQUES BRAGA <dorian.marques_braga@devinci.fr>
2019-02-05 10:20:40 +0100

    RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  constructor() {
-  }
+export class AppComponent implements OnInit, OnDestroy {
+
+  secondes: number;
+  counterSub: Subscription;
+
+  constructor() {}
 
   ngOnInit(): void {
+    const counter = interval(1000);
+    this.counterSub = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.counterSub.unsubscribe();
   }
 }
